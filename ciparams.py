8a16bc337df7dfe2988f74107b85a9ed2cc01dfe
Abel Navarro <abel.navarro@gmail.com>
2016-08-04 11:46:32 +0200

    some improvements

--- ciparams.py
+++ ciparams.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 
 import argparse
+import urllib2
+import ssl
+import re
+import string
 
 
 def main():
@@ -9,6 +13,36 @@ def main():
     args = parser.parse_args()
 
     print "build_number: {}".format(args.build_number)
+
+    # Build parameters page
+    url = "https://ci.suse.de/job/openstack-mkcloud/{}/parameters".format(args.build_number)
+    print "url: " + url
+
+    context = ssl._create_unverified_context()
+    data = urllib2.urlopen(url, context=context)
+    prevparam = 'cloudsource'
+    for line in data:
+        if not 'input readonly' in line:
+            continue
+
+        #print "line: " + line
+
+        value = line.split('"')[5]
+        elems = re.split('<|>', line)
+        param = ''
+        for elem in elems:
+            cleanelem = re.sub('[ |\n]', '', filter(lambda x: x in set(string.printable), elem))
+            if not cleanelem or re.match(r't.*|/t.*|input.*', cleanelem):
+                continue
+
+            param = cleanelem
+
+
+        if value:
+            print "export {}={}".format(prevparam, value)
+
+        prevparam = param
+
 
 if __name__ == '__main__':
     main()
